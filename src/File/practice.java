1862ee7ff5dcd24de01293f39c22b95991f8878b
dyxhhxx <15896883188@163.com>
2021-09-15 11:42:37 +0800

    test

--- src/File/practice.java
+++ src/File/practice.java
@@ -8,37 +8,53 @@ import java.io.IOException;
 public class practice {
 
     //复制文件，将源文件srcFile复制到目标文件destFile
-    public static void copyFile(File srcFile, File destFile){
-        byte[] temp1=new byte[(int)srcFile.length()];
-        byte[] temp2=new byte[(int)destFile.length()];
-        byte[] res=new byte[(int)(srcFile.length()+destFile.length())];
-        try(FileInputStream fis1=new FileInputStream(srcFile);
-            FileInputStream fis2=new FileInputStream(destFile);
-            FileOutputStream fos=new FileOutputStream(destFile)){
-            //现将两个文件读入两个字节数组
-            fis1.read(temp1);
-            fis2.read(temp2);
-            //再将两个数组拼成一个数组
-            for(int i=0;i<temp2.length;i++){
-                System.out.print(temp2[i]);
-                res[i]=temp2[i];
+//    public static void copyFile(File srcFile, File destFile){
+//        byte[] temp1=new byte[(int)srcFile.length()];
+//        byte[] temp2=new byte[(int)destFile.length()];
+//        byte[] res=new byte[(int)(srcFile.length()+destFile.length())];
+//        try(FileInputStream fis1=new FileInputStream(srcFile);
+//            FileInputStream fis2=new FileInputStream(destFile);
+//            FileOutputStream fos=new FileOutputStream(destFile)){
+//            //现将两个文件读入两个字节数组
+//            fis1.read(temp1);
+//            fis2.read(temp2);
+//            //再将两个数组拼成一个数组
+//            for(int i=0;i<temp2.length;i++){
+//                System.out.print(temp2[i]);
+//                res[i]=temp2[i];
+//            }
+//            System.out.println();
+//            for(int i=0;i<temp1.length;i++){
+//                System.out.print(temp1[i]);
+//                res[i+temp2.length]=temp1[i];
+//            }
+//            //将结果数组写入文件
+//            fos.write(res);
+//
+//        }catch(IOException e){
+//            e.printStackTrace();
+//        }
+//    }
+    public static void copyFile(String srcFilePath,String destFilePath){
+        File f1=new File(srcFilePath);
+        File f2=new File(destFilePath);
+        byte[] buffer=new byte[1024];
+        try(FileInputStream fis=new FileInputStream(f1);
+            FileOutputStream fos=new FileOutputStream(f2)){
+            while (true) {
+                int actuallyReaded = fis.read(buffer);
+                if (actuallyReaded == -1) {
+                    break;
+                }
+                fos.write(buffer,0,actuallyReaded);
+                fos.flush();
             }
-            System.out.println();
-            for(int i=0;i<temp1.length;i++){
-                System.out.print(temp1[i]);
-                res[i+temp2.length]=temp1[i];
-            }
-            //将结果数组写入文件
-            fos.write(res);
-
         }catch(IOException e){
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        File f1=new File("/Users/dingyx/LOL1.txt");
-        File f2=new File("/Users/dingyx/LOL-1.txt");
-        copyFile(f1,f2);
+        copyFile("/Users/dingyx/LOL1.txt","/Users/dingyx/LOL2.txt");
     }
 }
--- src/File/testByteStream.java
+++ src/File/testByteStream.java
@@ -10,29 +10,29 @@ public class testByteStream {
 
     public static void main(String[] args) {
 
-        //文件输入流FileInputStream
-        try{
-            //创建基于文件的输出流
-            File f=new File("/Users/dingyx/DOTA.exe");
-            //通过这个输出流，就可以把数据从硬盘，读取到Java的虚拟机中来，也就是读取到内存中
-            FileInputStream fis=new FileInputStream(f);
-            fis.close();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-
-        //文件输出流FileOutputStream
-        try{
-            File f1=new File("/Users/dingyx/DOTA.exe/LOL.txt");
-            FileOutputStream fos=new FileOutputStream(f1);
-            fos.close();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
+//        //文件输入流FileInputStream
+//        try{
+//            //创建基于文件的输出流
+//            File f=new File("/Users/dingyx/DOTA.exe");
+//            //通过这个输出流，就可以把数据从硬盘，读取到Java的虚拟机中来，也就是读取到内存中
+//            FileInputStream fis=new FileInputStream(f);
+//            fis.close();
+//        }catch(IOException e){
+//            e.printStackTrace();
+//        }
+//
+//        //文件输出流FileOutputStream
+//        try{
+//            File f1=new File("/Users/dingyx/DOTA.exe/LOL.txt");
+//            FileOutputStream fos=new FileOutputStream(f1);
+//            fos.close();
+//        }catch(IOException e){
+//            e.printStackTrace();
+//        }
 
         //以字节流的形式读取文件内容
         try{
-            File f0=new File("/Users/dingyx/LOL.txt");
+            File f0=new File("/Users/dingyx/LOL2.txt");
             FileInputStream fis0=new FileInputStream(f0);
             byte[] all=new byte[(int) f0.length()];
             fis0.read(all);
